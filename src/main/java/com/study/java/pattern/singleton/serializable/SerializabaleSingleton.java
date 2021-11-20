9bdf50ab5e383251a89855223937a4aeb70f152f
Nicholas Sun <799327210@qq.com>
2021-11-20 21:51:29 +0800

    init

--- src/main/java/com/study/java/pattern/singleton/serializable/SerializabaleSingleton.java
+++ src/main/java/com/study/java/pattern/singleton/serializable/SerializabaleSingleton.java
@@ -6,7 +6,11 @@ import java.io.Serializable;
  * 序列化破坏单例
  * 把内存中的对象的状态转换为字节码的形式
  * 把字节码通过IO输出流写入到磁盘上
- * 永久保存下来 
+ * 永久保存下来 ,持久化
+ *
+ * 反序列化
+ * 将持久化的字节码内容，通过IO输入流读到内存中
+ *
  */
 public class SerializabaleSingleton implements Serializable {
 
